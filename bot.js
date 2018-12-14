c901431ab6553f329a02f04099801c1defc4b44d
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-12-14 17:50:31 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3750,86 +3750,9 @@ if (message.content.startsWith("v!deletecolors")) {
 });
 })
 }})
-client.on("message", (message) => {
-
-  if(message.content.startsWith(`${prefix}new`)){
-     const reason = message.content.split(" ").slice(1).join(" ");
-     if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-     if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
-     message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
-         let role = message.guild.roles.find("name", "Support Team");
-         let role2 = message.guild.roles.find("name", "@everyone");
-         c.overwritePermissions(role, {
-             SEND_MESSAGES: true,
-             READ_MESSAGES: true
-         });
-         c.overwritePermissions(role2, {
-             SEND_MESSAGES: false,
-             READ_MESSAGES: false
-         });
-         c.overwritePermissions(message.author, {
-             SEND_MESSAGES: true,
-             READ_MESSAGES: true
-         });
-         message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
-         const embed = new Discord.RichEmbed()
-             .setColor(0xCF40FA)
-             .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
-             .setTimestamp();
-         c.send({
-             embed: embed
-         });
-     }).catch(console.error);
- }
 
 
-  if(message.content.startsWith(`${prefix}close`)){
-     if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
 
-     message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`v!close`. This will time out in 10 seconds and be cancelled.`)
-         .then((m) => {
-             message.channel.awaitMessages(response => response.content === 'v!close', {
-                     max: 1,
-                     time: 10000,
-                     errors: ['time'],
-                 })
-                 .then((collected) => {
-                     message.channel.delete();
-                 })
-                 .catch(() => {
-                     m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-                         m2.delete();
-                     }, 3000);
-                 });
-         });
- }
-
-});
-client.on('message',message =>{
-    if(message.content.startsWith(prefix + 'topinvites')) {
-  message.guild.fetchInvites().then(i =>{
-  var invites = [];
-   
-  i.forEach(inv =>{
-    var [invs,i]=[{},null];
-     
-    if(inv.maxUses){
-        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
-    }else{
-        invs[inv.code] =+ inv.uses;
-    }
-        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
-   
-  });
-  var embed = new Discord.RichEmbed()
-  .setColor("#000000")
-  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
-           message.channel.send({ embed: embed });
-   
-  });
-   
-    }
-  });
 client.on('message', message => {
 if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'move')) {
@@ -5115,8 +5038,6 @@ msg.channel.send(`${item.type}`).then(() => {
 :record_button: ~~__**Create a room named log to start the log**__~~ :record_button:
  
 :fire: __Action Commands:__
-❯ ${prefix}new → create ticket for you (need support-team role)
-❯ ${prefix}close → close the ticket
 ❯ ${prefix}slap → slap someone
 ❯ ${prefix}hug → hug someone
 ❯ ${prefix}tickle → tickle someone
